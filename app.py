eed17816485fa907ad92699cf707dc44e350dc85
Leo Yu <leo.yu@compass.com>
2018-06-26 09:40:07 -0400

    update hello endpoint

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def hello(path):
+    return "Hello World!\nPath: {}".format(path)
+
+
+if __name__ == '__main__':
+    app.run()
